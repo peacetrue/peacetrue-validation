96e68eb01dec6c07b51f0d050cf3b18c14184fca
xiayx <xiayouxue@hotmail.com>
2019-06-11 21:51:47 +0800

    调整MultiNotNull的验证逻辑

--- src/main/java/com/github/peacetrue/validation/constraints/multinotnull/MultiNotNullValidator.java
+++ src/main/java/com/github/peacetrue/validation/constraints/multinotnull/MultiNotNullValidator.java
@@ -1,5 +1,6 @@
 package com.github.peacetrue.validation.constraints.multinotnull;
 
+import com.github.peacetrue.lang.reflect.NoSuchPropertyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -7,10 +8,10 @@ import org.springframework.util.ReflectionUtils;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.beans.FeatureDescriptor;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * 验证一个对象的多个属性值至少有n个不能为null
@@ -21,13 +22,13 @@ public class MultiNotNullValidator implements ConstraintValidator<MultiNotNull, 
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    private Set<String> propertyNames;
+    private String[] propertyNames;
     private int count;
 
     @Override
     public void initialize(MultiNotNull constraintAnnotation) {
         logger.info("初始化注解[{}]", constraintAnnotation);
-        this.propertyNames = new HashSet<>(Arrays.asList(constraintAnnotation.propertyNames()));
+        this.propertyNames = constraintAnnotation.propertyNames();
         this.count = constraintAnnotation.count();
     }
 
@@ -37,15 +38,23 @@ public class MultiNotNullValidator implements ConstraintValidator<MultiNotNull, 
         if (value == null) return true;
 
         Class<?> valueClass = value.getClass();
-        PropertyDescriptor[] propertyDescriptors = BeanUtils.getPropertyDescriptors(valueClass);
+        PropertyDescriptor[] descriptors = BeanUtils.getPropertyDescriptors(valueClass);
+        if (propertyNames.length == 0) {
+            //设置所有属性，验证描述时需要使用
+            propertyNames = Arrays.stream(descriptors)
+                    //排除非bean的属性，例如：Object.getClass
+                    .filter(propertyDescriptor -> propertyDescriptor.getReadMethod() != null && propertyDescriptor.getWriteMethod() != null)
+                    .map(FeatureDescriptor::getName).toArray(String[]::new);
+            logger.debug("取得类[{}]的所有属性: {}", valueClass.getName(), propertyNames);
+        }
+
         int notNullCount = 0;
-        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-            //空属性默认使用所有属性
-            if (!propertyNames.isEmpty() && !propertyNames.contains(propertyDescriptor.getName())) continue;
-            //排除Class.getClass及其他非bean的属性
-            if (propertyDescriptor.getReadMethod() == null || propertyDescriptor.getWriteMethod() == null) continue;
-            Object propertyValue = ReflectionUtils.invokeMethod(propertyDescriptor.getReadMethod(), value);
-            logger.debug("取得属性[{}]的值为: {}", propertyDescriptor.getName(), propertyValue);
+        for (String propertyName : propertyNames) {
+            Method method = Arrays.stream(descriptors)
+                    .filter(propertyDescriptor -> propertyDescriptor.getName().equals(propertyName))
+                    .findAny().map(PropertyDescriptor::getReadMethod)
+                    .orElseThrow(() -> new NoSuchPropertyException(valueClass, propertyName));
+            Object propertyValue = ReflectionUtils.invokeMethod(method, value);
             if (propertyValue != null) notNullCount++;
         }
         logger.debug("取得 not null 值的数目[{}]", notNullCount);
