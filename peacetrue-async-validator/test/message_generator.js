1738ecde5cac4a9825e9dc85f8b77ffe3688cf58
peacetrue <xiayouxue@gmail.com>
2022-06-29 03:36:51 +0800

    删除 peacetrue-async-validator

--- peacetrue-async-validator/test/message_generator.js
+++
@@ -1,5 +0,0 @@
-let Message = require('../src/message_generator');
-
-let descriptor = {name: [{type: 'string'}, {required: true}, {min: 3, message: '至少 3个字符'}, {max: 8}]};
-Message.generate(descriptor, {name: '姓名'});
-console.info(descriptor)
